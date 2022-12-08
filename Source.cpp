b34714037aa7814b951716a21ae4f2a5243c866d
A-Alhammadi <113705421+A-Alhammadi@users.noreply.github.com>
2022-12-08 17:34:21 -0600

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -10,8 +10,14 @@ using namespace std;
 
 int main() {
 
-	move_to_rear();//call function
-
+	//move_to_rear();//call function
+	string s;
+	cout << "Please enter a string: " << endl;
+	cin >> s; //take in word to see if palindrome
+	if (palindrome(s))
+		cout << endl << "The string is a palindrome"; //if it is this will be printed
+	else
+		cout << endl << "The string is NOT a palindrome"; //if it is not this will be
 	return 0;
 }
 
@@ -29,5 +35,15 @@ void move_to_rear()
 	Q1.push(n); // adds n (which is whatever was the first element previously) to the end of the queue 
 	cout << endl << "Element at front of queue after function called: " << endl;
 	cout << Q1.front() << endl; //prints element now at the front
+
+}
+
+bool palindrome(string a) {
+	if (a.length() <= 1)
+		return true;                    //if the string is one letter (or none) then obviously it is the same right to left or left to right
+	if (a[0] == a[a.length() - 1])      //we start at the first character of the string (position 0) and compare with the last (whatever the lenght is minus 1)
+		return palindrome(a.substr(1, a.length() - 2)); //this is the recursive part we call the function again for the substring starting at the next position from both sides
+	else
+		return false;
 
 }
