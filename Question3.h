602a354dc70906d37f43340efbf380a6386bf281
A-Alhammadi <113705421+A-Alhammadi@users.noreply.github.com>
2022-12-08 18:54:34 -0600

    Create Question3.h

---
+++ Question3.h
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <queue>
+#include <string>
+#include <map>
+
+
+using namespace std;
+
+void move_to_rear();
+
+bool palindrome(string a);
+
+void printData(map<string, string> m);
